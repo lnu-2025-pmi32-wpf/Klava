111e1b36b6c7fab1f84edea7bdaa77a8b2eaebcb
stsvt <taras.stasiv3@gmail.com>
2025-12-10 23:57:18 +0200

    add reusable LoadingOverlay control and integrate into views

---
+++ src/Klava.WPF/Controls/LoadingOverlay.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Klava.WPF.Controls
+{
+    public partial class LoadingOverlay : UserControl
+    {
+        public LoadingOverlay()
+        {
+            InitializeComponent();
+        }
+
+        public static new readonly DependencyProperty IsVisibleProperty = DependencyProperty.Register(
+            nameof(IsVisible), typeof(bool), typeof(LoadingOverlay), new PropertyMetadata(false));
+
+        public new bool IsVisible
+        {
+            get => (bool)GetValue(IsVisibleProperty);
+            set => SetValue(IsVisibleProperty, value);
+        }
+
+        public static readonly DependencyProperty MessageProperty = DependencyProperty.Register(
+            nameof(Message), typeof(string), typeof(LoadingOverlay), new PropertyMetadata("Loading..."));
+
+        public string Message
+        {
+            get => (string)GetValue(MessageProperty);
+            set => SetValue(MessageProperty, value);
+        }
+    }
+}
